b4257e5d54849593127577e5dd2602c339d9d8b4
Nerisson <jean-paul.sky@laposte.net>
2015-02-12 20:52:11 +0100

    KATA #1

---
+++ Kata/KATA1/main.cpp
@@ -0,0 +1,43 @@
+#include <windows.h>
+#include <GL/glut.h>
+GLfloat RED[] =  {1., 0., 0., 1.};
+GLfloat GREEN[] = {0., 1., 0., 1.};
+GLfloat BLUE[] = {0., 0., 1., 1.};
+GLfloat BLACK[] = {0., 0., 0., 1.};
+GLfloat WHITE[] = {1., 1., 1., 1.};
+
+int main(int argc, char **argv)
+{
+    void initialize(char *);
+    void display(void);
+    glutInit(&argc, argv) ; /*Initiase GLUT et traite les arguments de la ligne de commande*/
+    initialize(argv[0]);
+    glutDisplayFunc(display) ;
+    glutMainLoop() ;
+    return 0 ;
+}
+
+void initialize(char *title)
+{
+    void defineWindow(char *);
+    glutInitDisplayMode(GLUT_RGB) ;
+    defineWindow(title);
+}
+
+void defineWindow(char *title)
+{
+    GLsizei screenWidth, screenHeight  ;
+    screenWidth= glutGet(GLUT_SCREEN_WIDTH);
+    screenHeight  = glutGet(GLUT_SCREEN_HEIGHT);
+	glutInitWindowPosition(screenWidth/4, screenHeight  /4);  glutInitWindowSize(screenWidth/2, screenHeight  /2);
+    glutCreateWindow(title) ;
+}
+
+
+void display(void)
+{
+    glClear(GL_COLOR_BUFFER_BIT) ;
+    glColor4fv(RED);
+    glutSolidTeapot(.5);
+    glFlush() ;
+}
