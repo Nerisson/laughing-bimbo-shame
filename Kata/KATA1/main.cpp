4c155dba826d6f98dc7f32865c854b2b175d8916
Nerisson <jean-paul.sky@laposte.net>
2015-02-12 23:39:48 +0100

    Premiers Test

--- Kata/KATA1/main.cpp
+++ Kata/KATA1/main.cpp
@@ -1,13 +1,19 @@
 #include <windows.h>
 #include <GL/glut.h>
+
+#include <iostream>
+
+using namespace std;
+
+void reshape (int w, int h);
+
 GLfloat RED[] =  {1., 0., 0., 1.};
 GLfloat GREEN[] = {0., 1., 0., 1.};
 GLfloat BLUE[] = {0., 0., 1., 1.};
 GLfloat BLACK[] = {0., 0., 0., 1.};
 GLfloat WHITE[] = {1., 1., 1., 1.};
 
-int main(int argc, char **argv)
-{
+int main(int argc, char **argv){
     void initialize(char *);
     void display(void);
     glutInit(&argc, argv) ; /*Initiase GLUT et traite les arguments de la ligne de commande*/
@@ -17,27 +23,34 @@ int main(int argc, char **argv)
     return 0 ;
 }
 
-void initialize(char *title)
-{
+void initialize(char *title){
     void defineWindow(char *);
+    glutInitDisplayMode(GLUT_RGB) ;
     glutInitDisplayMode(GLUT_RGB) ;
     defineWindow(title);
 }
 
-void defineWindow(char *title)
-{
+void defineWindow(char *title){
     GLsizei screenWidth, screenHeight  ;
     screenWidth= glutGet(GLUT_SCREEN_WIDTH);
     screenHeight  = glutGet(GLUT_SCREEN_HEIGHT);
 	glutInitWindowPosition(screenWidth/4, screenHeight  /4);  glutInitWindowSize(screenWidth/2, screenHeight  /2);
     glutCreateWindow(title) ;
+    glutReshapeFunc(reshape);
 }
 
 
-void display(void)
-{
+void display(void){
     glClear(GL_COLOR_BUFFER_BIT) ;
     glColor4fv(RED);
     glutSolidTeapot(.5);
     glFlush() ;
+}
+
+void reshape (int w, int h){
+   cout << "Hello: width:" << w << " height:" << h << endl;
+    glLoadIdentity();
+    gluPerspective(65.0, (GLfloat) w/ (GLfloat) h, 1.0, 20.0);
+    glMatrixMode(GL_MODELVIEW);
+    glLoadIdentity();
 }
---
+++ Kata/Tuto/TestClavier/main.cpp
@@ -0,0 +1,150 @@
+#include <windows.h>
+#include <iostream>
+#include <GL/glut.h>
+
+
+using namespace std;
+
+double rotAngle = 10;
+
+GLfloat trans[] =  {0., 0., 0.,};
+
+
+void init()
+{
+    glClearColor(0, 0, 0, 0);		// background color
+    glClearDepth(1.0);			// background depth value
+
+    glMatrixMode(GL_PROJECTION);
+    glLoadIdentity();
+    gluPerspective(60, 1, 1, 1000);	// setup a perspective projection
+
+    glMatrixMode(GL_MODELVIEW);
+    glLoadIdentity();
+
+    gluLookAt(				// set up the camera
+		0.0, 0.0, 5.0,		// eye position
+		0.0, 0.0, 0.0,		// lookat position
+		0.0, 1.0, 0.0);		// up direction
+
+    glEnable(GL_DEPTH_TEST);		// enable hidden surface removal
+
+    glEnable(GL_LIGHTING);		// enable lighting
+    glEnable(GL_LIGHT0);		// enable
+
+    float lpos[] = { 5, 5, 5, 0 };
+    glLightfv(GL_LIGHT0, GL_POSITION, lpos);
+
+    // glShadeModel(GL_FLAT);		// flat shading
+    glShadeModel(GL_SMOOTH);		// smooth shading
+}
+
+//-----------------------------------------------------------------------
+// display callback function
+//	This is called each time application needs to redraw itself.
+//	Most of the opengl work is done through this function.
+//-----------------------------------------------------------------------
+
+void display()
+{
+    glClear(
+	GL_COLOR_BUFFER_BIT |		// clear the frame buffer (color)
+	GL_DEPTH_BUFFER_BIT);		// clear the depth buffer (depths)
+
+
+
+    glPushMatrix();			// save the current camera transform
+
+    glRotated(rotAngle, 0, 1, 0);	// rotate by rotAngle about y-axis
+    glTranslatef(trans[0], trans[1], trans[2]);
+
+    glEnable(GL_COLOR_MATERIAL);	// specify object color
+    glColor3f(1.0, 0.1, 0.1);		// redish
+
+    glutSolidTeapot(1);			// draw the teapot
+
+    glPopMatrix();			// restore the modelview matrix
+    glFlush();				// force OpenGL to render now
+
+    glutSwapBuffers();			// make the image visible
+}
+
+//-----------------------------------------------------------------------
+// keyboard callback function
+//	This is called whenever a keyboard key is hit.
+//-----------------------------------------------------------------------
+
+void keyboard(unsigned char k, int x, int y)
+{
+    cout << "keyboard:" << k << endl;
+    switch (k)
+    {
+    case 'q':
+        trans[0] -= 0.1;			// increase rotation by 5 degrees
+	break;
+    case 'd':
+        trans[0] += 0.1;			// decrease rotation by 5 degrees
+	break;
+   case 'z':
+        trans[2] += 0.1;			// decrease rotation by 5 degrees
+	break;
+   case 's':
+        trans[2] -= 0.1;			// decrease rotation by 5 degrees
+       case 'a':
+        rotAngle += 5;			// decrease rotation by 5 degrees
+	break;
+   case 'e':
+        rotAngle -= 5;			// decrease rotation by 5 degrees
+
+
+	break;
+        case 27:
+	exit(0);			// exit
+    }
+
+    glutPostRedisplay();		// redraw the image now
+}
+
+//-----------------------------------------------------------------------
+//  usage
+//	Explain how to use program.
+//-----------------------------------------------------------------------
+
+void usage()
+{
+    cout << "\n\
+-----------------------------------------------------------------------\n\
+  CMSC 427 Sample Program.\n\
+  Inputs:\n\
+    a:              Rotate counterclockwise\n\
+    l:              Rotate clockwise\n\
+    q:              Quit\n\
+  You may need to place the cursor over the graphics window for\n\
+  keyboard input to be processed.\n\
+-----------------------------------------------------------------------\n";
+    cout.flush();
+}
+
+//-----------------------------------------------------------------------
+// main program
+//	Where everything begins.
+//-----------------------------------------------------------------------
+
+int main()
+{
+    usage();				// explain how to use
+    glutInitDisplayMode(		// initialize GLUT
+		GLUT_DOUBLE |		// use double buffering
+		GLUT_DEPTH |		// request memory for z-buffer
+		GLUT_RGB );		// set RGB color mode
+
+    glutCreateWindow("GLUT Example");	// create the window
+
+    glutDisplayFunc(display);		// call display() to redraw window
+    glutKeyboardFunc(keyboard);		// call keyboard() when key is hit
+
+    init();				// our own initializations
+
+    glutMainLoop();			// let GLUT take care of everything
+    return 0;
+}
